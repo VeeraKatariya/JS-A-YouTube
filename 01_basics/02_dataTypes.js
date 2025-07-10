8cabb66d325a9fa43e24330441cd4caa8290356d
VeeraKatariya <aanchalkatariyya@gmail.com>
2025-07-10 05:06:14 +0000

    Commit Basic File

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,38 @@
+// "use strict";  Treat all js code as newer version
+
+// alert ("Hello")  We are using nodejs, not browser
+
+// console.log(3+3)
+// console.log("Aanchal");
+
+let name ="Aanchal"
+let age = 22
+let isLoggedIn = false
+let temp = null
+let state = undefined
+
+
+// number => 2 to the power 53
+// bigint = very big number
+// string = Set of Character
+// Boolean = True/False
+// Null = Standalone Value 
+// Symbol = Uniqeness value
+// undefined = not a assign value
+
+
+//  Object 
+
+// console.log(typeof "Aanchal");
+console.log(typeof age);
+
+console.log(typeof isLoggedIn);
+
+console.log(typeof null);
+
+console.log(typeof state);
+
+
+
+
+
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,41 @@
+// let score = "22"
+// let score = "22abs"
+// let score = null
+// let score = undefined
+// let score = true
+let score = "Aanchal"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+// "22"=> 33
+// "22abs" => NaN
+// true => 1; false => 0
+
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = "Aanchal"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Aanchal" => true
+
+
+let anyNumber = 33
+
+let stringNumber = String(anyNumber)
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
+
+
+
+
