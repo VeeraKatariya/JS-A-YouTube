466c884ce4aafe39b0540947e0db263dd3f3f0f1
VeeraKatariya <aanchalkatariyya@gmail.com>
2025-07-11 09:37:55 +0000

    Ubdate commit

--- 01_basics/04_Operation.js
+++ 01_basics/04_Operation.js
@@ -36,17 +36,51 @@ let str3 = str1 + str2
 // Increase the value by one but it return the original value 
 let num1 = 2
 let num2 = num1++
-console.log(num1);
-console.log(num2);
+// console.log(num1);
+// console.log(num2);
 
 
 // ******************* PREFIX INCREMENT ********************
 // Increase the value by one and it return the new increamental value 
 let value = 45
 let value1 = ++value
-console.log(value);
+// console.log(value);
 
-console.log(value1);
+// console.log(value1);
+
+
+
+// ********************* Comparision ****************************
+
+// console.log(2>1);
+// console.log(3<5);
+
+// console.log(2==1);
+// console.log(2>=1);
+// console.log(2!=1);
+
+// console.log("2" > 1);       2 convert in number by javascript 
+// console.log("02" > 1);
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined < 0);
+
+
+console.log("2" === 2);     /*Conversion not allowed 
+Check data type with strictly */
+
+
+
+
+
+
+
 
 
 
