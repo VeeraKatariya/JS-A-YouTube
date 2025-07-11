f5f846710dee2c57b888840686e6335adcfae230
VeeraKatariya <aanchalkatariyya@gmail.com>
2025-07-11 06:56:08 +0000

    Commit the file

---
+++ 01_basics/04_Operation.js
@@ -0,0 +1,62 @@
+
+
+
+// **************************** OPERATIONS ******************************
+
+
+
+let num = 3
+let negvalue = -num
+// console.log(negvalue);
+
+// console.log(3+3);
+// console.log(2-2);
+// console.log(3*3);
+// console.log(4**4);
+// console.log(4%2);
+// console.log(4/4);
+
+
+let str1 = "Hello"
+let str2 = " Aanchal"
+let str3 = str1 + str2
+// console.log(str3)
+
+
+// console.log("2" + 3);
+// console.log(1 + "3");
+// console.log("2" + "3")
+// console.log("1" +2 +3);
+// console.log(1 + 2 + "3");
+
+
+
+
+// ******************** POSTFIX INCREMENT ********************
+// Increase the value by one but it return the original value 
+let num1 = 2
+let num2 = num1++
+console.log(num1);
+console.log(num2);
+
+
+// ******************* PREFIX INCREMENT ********************
+// Increase the value by one and it return the new increamental value 
+let value = 45
+let value1 = ++value
+console.log(value);
+
+console.log(value1);
+
+
+
+
+
+
+
+
+
+
+
+
+
