ad3b74f8c1a486b71141e0e761ec84c1b8ae8cf7
VeeraKatariya <aanchalkatariyya@gmail.com>
2025-08-14 19:31:41 +0530

    commit some file

---
+++ 05_iterations/02_whileloop.js
@@ -0,0 +1,22 @@
+
+let index = 0
+
+while (index <= 10) {
+    // console.log(`value of index is ${index}`);
+    index = index +2
+}
+
+
+let myArray = ["aanchal", "ritika", "aarav"]
+let arr = 0
+while (arr<myArray.length) {
+    // console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+
+let value = 11
+do {
+  console.log(`value is ${value}`);
+    value++
+} while (value <= 10);
